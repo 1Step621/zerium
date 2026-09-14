a5a2abe73613fe9349d3cba164e8087886d6f09f
1Step621 <onestep621@gmail.com>
2026-09-15 02:59:47 +0900

    wip

--- packaging/linux/package.sh
+++ packaging/linux/package.sh
@@ -15,6 +15,26 @@ find target/ffmpeg-sdk/lib -type f -name '*.so.*' -exec patchelf --set-rpath '$O
 cp target/release/zerium "$bundle_dir/bin/zerium"
 cp -a target/ffmpeg-sdk/lib/. "$bundle_dir/lib/"
 cp LICENSE "$bundle_dir/LICENSE"
-tar --create --xz --file "dist/$archive_name" --directory "$(dirname "$bundle_dir")" "$(basename "$bundle_dir")"
-cargo deb --no-build --output "dist/zerium_${version}-1_amd64.deb"
-cargo generate-rpm -o "dist/zerium-${version}-1.x86_64.rpm"
+tar --create --use-compress-program='xz -T0 -6' \
+  --file "dist/$archive_name" \
+  --directory "$(dirname "$bundle_dir")" "$(basename "$bundle_dir")"
+
+deb_log="${RUNNER_TEMP:-/tmp}/zerium-cargo-deb.log"
+rpm_log="${RUNNER_TEMP:-/tmp}/zerium-cargo-rpm.log"
+cargo deb --no-build --output "dist/zerium_${version}-1_amd64.deb" >"$deb_log" 2>&1 &
+deb_pid=$!
+cargo generate-rpm --payload-compress gzip -o "dist/zerium-${version}-1.x86_64.rpm" >"$rpm_log" 2>&1 &
+rpm_pid=$!
+
+deb_status=0
+rpm_status=0
+wait "$deb_pid" || deb_status=$?
+wait "$rpm_pid" || rpm_status=$?
+if ((deb_status != 0)); then
+  cat "$deb_log" >&2
+  exit "$deb_status"
+fi
+if ((rpm_status != 0)); then
+  cat "$rpm_log" >&2
+  exit "$rpm_status"
+fi
